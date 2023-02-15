d49c2a087e9b6bb7578601bf2ffdc2b93a4f38c1
D-Jy <duan@d-jy.net>
2023-02-16 03:59:55 +0800

    精简体积，优化输出

--- chat.py
+++ chat.py
@@ -1,6 +1,5 @@
 from revChatGPT.V1 import Chatbot
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Body
 import uvicorn
 
 app = FastAPI()
@@ -9,40 +8,40 @@ HOST = "0.0.0.0"
 PORT = 8006
 
 chatbot = Chatbot(config={
-  "email": "<your email>",
-  "password": "your password"
+    "email": "<your email>",
+    "password": "your password"
 })
 
-class ChatRequest(BaseModel):
-  prompt: str
 
 @app.get("/ping")
 def ping():
-  return {"message": "pong"}
+    return {"message": "pong"}
+
 
 @app.post("/chat")
-async def chatGPT(request: ChatRequest):
-  prompt = request.prompt
-  print(prompt)
-  if prompt == "__clear__":
-    chatbot.reset()
-    return {"message": "OK"}
-  answer = ""
-  try:
-    for data in chatbot.ask(
-      prompt,
-      conversation_id=chatbot.config.get("conversation"),
-      parent_id=chatbot.config.get("parent_id"),
-    ):
-      #print(data["message"], end="", flush = True)
-      answer = data["message"]
-    print(data["message"])
-    print()
-  except:
-    answer = "ERROR"
-    pass
-  return {"message": answer}
+async def chatGPT(body: dict = Body(...)):
+    prompt = body["prompt"]
+    print("User: " + prompt)
+    print("Chatbot: ", end="")
+    try:
+        if prompt == "!reset":
+            chatbot.reset_chat()
+            print("OK")
+            return {"message": "OK"}
+        answer = ""
+        for data in chatbot.ask(
+            prompt,
+            conversation_id=chatbot.config.get("conversation"),
+            parent_id=chatbot.config.get("parent_id"),
+        ):
+            message = data["message"][len(answer):]
+            print(message, end="", flush=True)
+            answer = data["message"]
+        print()
+    except:
+        answer = "ERROR"
+        pass
+    return {"message": answer}
 
 if __name__ == "__main__":
-  uvicorn.run(app, host=HOST, port=PORT)
-
+    uvicorn.run(app, host=HOST, port=PORT)
